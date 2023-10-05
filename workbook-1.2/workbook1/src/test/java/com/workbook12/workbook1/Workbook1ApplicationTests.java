e835e8c11615e4899220a0ddf12e911bc2b5d48f
Jeffrey <jeffreygrahambusiness@gmail.com>
2023-10-05 09:00:17 -0400

    pushing spring boot course

---
+++ workbook-1.2/workbook1/src/test/java/com/workbook12/workbook1/Workbook1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workbook12.workbook1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workbook1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
